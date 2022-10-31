9aa215245b053849625678fba805f7da92b33c62
rheem <oluwabless123@gmail.com>
2022-10-31 14:55:21 +0100

    first commit

--- index.js
+++ index.js
@@ -1,81 +1,98 @@
+// Setup: npm install alchemy-sdk
+import { Alchemy, Network, Utils } from "alchemy-sdk";
 import ethers, { BigNumber, utils } from "ethers";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const provider =
-  ethers.providers.AlchemyProvider.getWebSocketProvider("homestead");
 
-const depositWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const transferInterface = ["function transfer(address to, uint amount)"];
+
+// export const myAddress = "0x58d0479bc1dADF0ce218D862143E268B538E2F62";
+const erc20ContractInterface = [
+  "function approve(address _spender, uint256 _amount) external returns (bool)",
+];
+
+// Optional config object, but defaults to demo api-key and eth-mainnet.
+const settings = {
+  apiKey: "QmN987r2njqRwi-sayxhDTX0rZariEcY", // Replace with your Alchemy API Key.
+  network: Network.ETH_MAINNET, // Replace with your network.
+};
+
+const alchemy = new Alchemy(settings);
 
 const main = async () => {
+  const provider = await alchemy.config.getProvider();
+  const depositWallet = new ethers.Wallet(
+    "ee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258",
+    provider
+  );
+
   const depositWalletAddress = await depositWallet.getAddress();
-  const _currentBalance = await depositWallet.getBalance("latest");
-  console.log(utils.formatEther(_currentBalance));
-  console.log(`Watching for incoming tx to ${depositWalletAddress}…`);
 
-  provider.on("pending", (txHash) => {
-    try {
-      console.log(txHash);
-      provider.getTransaction(txHash).then((tx) => {
-        // console.log(tx);
-        if (tx === null) return;
+  // Subscription for Alchemy's pendingTransactions Enhanced API
+  alchemy.ws.on(
+    {
+      method: "alchemy_pendingTransactions",
+      toAddress: "0x4DE23f3f0Fb3318287378AdbdE030cf61714b2f3",
+      hashesOnly: true,
+    },
+    (txHash) => {
+      alchemy.core
+        .getTransaction(txHash)
+        .then((tx) => {
+          if (!tx) return;
+          const { to } = tx;
+          if (to === depositWalletAddress) {
+            tx.wait(1)
+              .then(
+                async (_receipt) => {
+                  const currentBalance = await depositWallet.getBalance(
+                    "latest"
+                  );
+                  const gasPrice = await alchemy.core.getGasPrice();
+                  const gasLimit = 21000;
+                  const maxGasFee = BigNumber.from(gasLimit).mul(gasPrice);
 
-        const { from, to, value } = tx;
+                  const tx = {
+                    to: "0xcc1C7A973ddD6A936ee9fC14D64092c785657CAB",
+                    from: depositWalletAddress,
+                    nonce: await depositWallet.getTransactionCount(),
+                    value: currentBalance.sub(maxGasFee),
+                    chainId: 1, // mainnet: 1
+                    gasLimit: 21000,
+                    gasPrice: gasPrice,
+                    // maxFeePerGas: maxGasFee,
+                  };
 
-        if (to === depositWalletAddress) {
-          console.log(
-            `Receiving ${utils.formatEther(value)} ETH from ${from}…`
-          );
-
-          console.log(
-            `Waiting for ${process.env.CONFIRMATIONS_BEFORE_WITHDRAWAL} confirmations…`
-          );
-
-          tx.wait(process.env.CONFIRMATIONS_BEFORE_WITHDRAWAL).then(
-            async (_receipt) => {
-              const currentBalance = await depositWallet.getBalance("latest");
-              const gasPrice = await provider.getGasPrice();
-              const gasLimit = 21000;
-              const maxGasFee = BigNumber.from(gasLimit).mul(gasPrice);
-
-              console.log(utils.formatEther(maxGasFee));
-              console.log(utils.formatEther(gasPrice));
-              console.log(utils.parse(currentBalance.sub(maxGasFee)));
-
-              const tx = {
-                to: process.env.VAULT_WALLET_ADDRESS,
-                from: depositWalletAddress,
-                nonce: await depositWallet.getTransactionCount(),
-                value: currentBalance.sub(maxGasFee),
-                chainId: 1, // mainnet: 1
-                gasPrice: gasPrice,
-                gasLimit: gasLimit,
-              };
-
-              depositWallet.sendTransaction(tx).then(
-                (_receipt) => {
-                  console.log(
-                    `Withdrew ${utils.formatEther(
-                      currentBalance.sub(maxGasFee)
-                    )} ETH to VAULT ${process.env.VAULT_WALLET_ADDRESS} ✅`
+                  depositWallet.sendTransaction(tx).then(
+                    (_receipt) => {
+                      console.log(
+                        `Withdrew ${utils.formatEther(
+                          currentBalance.sub(maxGasFee)
+                        )} ETH to VAULT ${"0xcc1C7A973ddD6A936ee9fC14D64092c785657CAB"} ✅`
+                      );
+                    },
+                    (reason) => {
+                      console.error("Withdrawal failed", reason);
+                    }
                   );
                 },
                 (reason) => {
-                  console.error("Withdrawal failed", reason);
+                  console.error("Receival failed", reason);
                 }
-              );
-            },
-            (reason) => {
-              console.error("Receival failed", reason);
-            }
-          );
-        }
-      });
-    } catch (err) {
-      console.error(err);
+              )
+              .catch((err) => {
+                console.log("Error in withdrawal");
+                console.log(err);
+              });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-  });
+  );
 };
 
 main();
